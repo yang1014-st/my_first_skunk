45c3069f2ea9a6cebea1e20a91986ccd6ad1f182
yang1014-st <yang1014@stthomas.edu>
2020-12-21 00:50:44 -0600

    Extract Method - isRegularSkunk()

--- H6_Skunk/src/SkunkDomain.java
+++ H6_Skunk/src/SkunkDomain.java
@@ -81,7 +81,7 @@ public class SkunkDomain
 					wantsToRoll = false;
 					break;
 				}
-				else if (skunkDice.getDie1().getLastRoll() == 1 || skunkDice.getDie2().getLastRoll() == 1)
+				else if (isRegularSkunk()) // Difficult to read
 				{
 					ui.println("One Skunk! You lose the turn, zeroing out the turn score and paying 1 chip to the kitty");
 					kitty += 1;
@@ -168,7 +168,7 @@ public class SkunkDomain
 					wantsToRoll = false;
 
 				}
-				else if (skunkDice.getDie1().getLastRoll() == 1 || skunkDice.getDie2().getLastRoll() == 1)
+				else if (isRegularSkunk())
 				{
 					ui.println("One Skunk!  You lose the turn, zeroing out the turn score and paying 1 chip to the kitty");
 					kitty += 1;
@@ -237,6 +237,10 @@ public class SkunkDomain
 
 		ui.println("-----------------------");
 		return true;
+	}
+
+	private boolean isRegularSkunk() {
+		return skunkDice.getDie1().getLastRoll() == 1 || skunkDice.getDie2().getLastRoll() == 1;
 	}
 
 	private boolean isSkunkDeuce() {
